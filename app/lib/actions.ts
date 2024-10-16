f7ee77f82df4587907302d5fd83c50fd1b4b9eaf
Chanice Irungu <chaniceashley@gmail.com>
2024-10-16 12:39:27 +0300

    ft: adding authentication and authorization

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,27 @@
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth'
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
  
 export async function createInvoice(formData: FormData) {
 
@@ -14,7 +35,7 @@ export async function createInvoice(formData: FormData) {
 
   const date = new Date().toISOString().split('T')[0];
   const amountInCents = Number(amount) * 100;
-  
+
   await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
@@ -23,8 +44,31 @@ export async function createInvoice(formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-// Tip: If you're working with forms that have many fields,
-//  you may want to consider using the entries() method 
-// with JavaScript's Object.fromEntries(). For example:
+// Use Zod to update the expected types
+// const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const customerId = formData.get('customerId') as string ;
+  const amount = formData.get('amount')
+  const status = formData.get('status') as string;
+  
+ 
+  const amountInCents = Number(amount) * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
 
-// const rawFormData = Object.fromEntries(formData.entries())
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
